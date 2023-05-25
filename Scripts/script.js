95e5cda2ac0d99dd7589b8900fc61fe06dcfc059
Your Name <1234avadhootsmart@gmail.com>
2023-05-25 19:09:05 +0530

    login btn function added

--- Scripts/script.js
+++ Scripts/script.js
@@ -5,6 +5,10 @@ let slide1 = document.getElementById('Porsche')
 let slide2 = document.getElementById('M4')
 let slide3 = document.getElementById('Mstng')
 let counter = 0
+let login = document.getElementById('login')
+
+
+
 slides.push(slide1)
 slides.push(slide2)
 slides.push(slide3)
@@ -13,20 +17,20 @@ slides.forEach(
     (Poster, index) => {
         Poster.style.left = `${index * 100}%`
     });
-
-slidesimg = () => {
-    slides.forEach(
-        (Poster) => {
-            Poster.style.transform = `translateX(-${counter * 100}%)`
-
-        });
+    
+    slidesimg = () => {
+        slides.forEach(
+            (Poster) => {
+                Poster.style.transform = `translateX(-${counter * 100}%)`
+                
+            });
 
 }
 resetcountnxt = () => {
     if (counter >= 2) {
         counter = -1
     }
-
+    
 }
 
 resetcountpre = () => {
@@ -34,7 +38,7 @@ resetcountpre = () => {
         counter = 2
     }
 }
-   
+
 
 
 
@@ -43,7 +47,7 @@ nxtslide = () => {
     // console.log("pre " + counter)
     counter++
     // console.log("post " + counter)
-
+    
     slidesimg()
     resetcountnxt()
 }
@@ -51,7 +55,7 @@ nxtslide = () => {
 prevslide = () => {
     // console.log(counter + " pre")
     resetcountpre()
-    counter--    
+    counter--
     slidesimg()
     // console.log(counter + " post")
 }
@@ -70,11 +74,19 @@ resetcountpre = () => {
         counter = 3
     }
 }
+// Login button event listener
+let pop = document.getElementsByClassName('popup')[0];
+let popLogin = document.getElementById('btn-popLogin')
 
+login.addEventListener('click', () => {
+    pop.style.opacity = 1;
+    pop.style.zIndex = 999;
+})
 
-
-
-
+popLogin.addEventListener('click', () => {
+    pop.style.zIndex = -2;
+    pop.style.opacity = 0;
+})
 
 
 
